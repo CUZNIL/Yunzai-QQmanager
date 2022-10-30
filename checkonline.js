c746e97ca6b4cb6411dffba1a53558d89f1bfc3a
硫酸钡Barite <iamliujunxi@qq.com>
2022-10-30 05:40:40 +0000

    update checkonline.js. 网络抖动过于频繁时不覆盖空账号，以免懵逼
    Signed-off-by: 硫酸钡Barite <iamliujunxi@qq.com>

--- checkonline.js
+++ checkonline.js
@@ -9,52 +9,54 @@ const require = createRequire(import.meta.url)
 const { exec } = require('child_process')
 
 export default class onlineEvent extends EventListener {
-    constructor () {
-      super({ event: 'system.offline' })
-      this.key = 'restart'
-      this.keys = 'setrestart'
+    constructor() {
+        super({ event: 'system.offline' })
+        this.key = 'restart'
+        this.keys = 'setrestart'
     }
 
-  
+
     /** 默认方法 */
-    async execute (e) {
-      let setrestart = await redis.get(this.keys)
-      if(!setrestart){
-        return true
-      }
-      let cm='npm run restart'
-      let asd = this.get('bot')
-      let deFault =fs.readFileSync(`${_path}/resources/Default.yaml`,'utf-8')
-      for(let i in asd){
-        if (fs.existsSync(`${_path}/data/${asd[i].qq[0]}/token`)){
-          deFault = deFault.replace(/qq:/g,'qq: '+ asd[i].qq[0])
-          deFault = deFault.replace(/pwd:/g,`pwd:  '${asd[i].pw}'`)
-          deFault = deFault.replace(/platform:/g,'platform: ' + asd[i].plat[0])
-          break
+    async execute(e) {
+        let setrestart = await redis.get(this.keys)
+        if (!setrestart) {
+            return true
         }
-      }
-      fs.writeFileSync(`${_path}/config/config/qq.yaml`, deFault, 'utf8')
-      console.log("开始重启")
-      await redis.set(this.key, '1')
-      exec(cm, { windowsHide: true }, (error, stdout, stderr) => {
-        if (error) {
-          redis.del(this.key)
-          logger.error(`重启失败\n${error.stack}`)
-        }else if (stdout) {
-          logger.mark('重启成功，运行已由前台转为后台')
-          logger.mark('查看日志请用命令：npm run log')
-          logger.mark('停止后台运行命令：npm stop')
-          process.exit()
+        let cm = 'npm run restart'
+        let asd = this.get('bot')
+        let deFault = fs.readFileSync(`${_path}/resources/Default.yaml`, 'utf-8')
+        for (let i in asd) {
+            if (fs.existsSync(`${_path}/data/${asd[i].qq[0]}/token`)) {
+                changed = true
+                deFault = deFault.replace(/qq:/g, 'qq: ' + asd[i].qq[0])
+                deFault = deFault.replace(/pwd:/g, `pwd:  '${asd[i].pw}'`)
+                deFault = deFault.replace(/platform:/g, 'platform: ' + asd[i].plat[0])
+                //修改了代码，仅当能查找到有token的账号时覆盖qq.yaml，否则不切换直接重启
+                fs.writeFileSync(`${_path}/config/config/qq.yaml`, deFault, 'utf8')
+                break
+            }
         }
-      })
+        console.log("开始重启")
+        await redis.set(this.key, '1')
+        exec(cm, { windowsHide: true }, (error, stdout, stderr) => {
+            if (error) {
+                redis.del(this.key)
+                logger.error(`重启失败\n${error.stack}`)
+            } else if (stdout) {
+                logger.mark('重启成功，运行已由前台转为后台')
+                logger.mark('查看日志请用命令：npm run log')
+                logger.mark('停止后台运行命令：npm stop')
+                process.exit()
+            }
+        })
     }
 
-    get (name) {
-      let file = `${_path}/resources/${name}.yaml`
-      let key = `${name}`
-      this[key] = YAML.parse(
-        fs.readFileSync(file, 'utf8')
-      )
-      return this[key]
+    get(name) {
+        let file = `${_path}/resources/${name}.yaml`
+        let key = `${name}`
+        this[key] = YAML.parse(
+            fs.readFileSync(file, 'utf8')
+        )
+        return this[key]
     }
-  }
+}
